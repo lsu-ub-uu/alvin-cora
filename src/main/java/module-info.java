0cbef8abf5d264521598fb6ad51ba96d04b82633
olovm <olov.mckie@ub.uu.se>
2022-04-08 15:13:18 +0000

    updating to java 17 (and other dependencies), using basicstorage

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,3 @@
-import se.uu.ub.cora.alvin.extendedfunctionality.AlvinExtendedFunctionalityFactory;
-import se.uu.ub.cora.spider.extendedfunctionality.ExtendedFunctionalityFactory;
-
 module se.uu.ub.cora.alvin {
-	requires se.uu.ub.cora.spider;
-	requires se.uu.ub.cora.storage;
 
-	provides ExtendedFunctionalityFactory with AlvinExtendedFunctionalityFactory;
 }
